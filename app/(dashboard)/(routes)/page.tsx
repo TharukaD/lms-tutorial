62ecbbd2f1a46952dd9076b1b0ccc6b5f635d387
Tharuka Deshan <tharuka1993deshan@gmail.com>
2023-09-30 17:47:30 +0500

    Initial

---
+++ app/(dashboard)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+import { User } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
