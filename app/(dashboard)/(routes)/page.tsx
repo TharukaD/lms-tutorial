f0802825eca1ba8c4e59054203863c491dad8cd6
Tharuka Deshan <tharuka1993deshan@gmail.com>
2023-10-01 10:28:44 +0500

    Added db models and ui changes

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -2,9 +2,5 @@ import { UserButton } from "@clerk/nextjs";
 import { User } from "lucide-react";
 
 export default function Home() {
-  return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div>Dashboard</div>;
 }
