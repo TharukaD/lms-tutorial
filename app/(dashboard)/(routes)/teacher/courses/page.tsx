7296ebcfd565b9e24bc0caa2112bacd475daabce
Tharuka Deshan <tharuka1993deshan@gmail.com>
2023-10-06 09:35:17 +0500

    Couse list page completed

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,28 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import React from "react";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const CoursesPage = () => {
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/courses/create">
-        <Button>New Course</Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
