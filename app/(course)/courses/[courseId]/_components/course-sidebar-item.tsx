bfb5384d32c6b912c4c4897586326670d6d148dd
Tharuka Deshan <tharuka1993deshan@gmail.com>
2023-10-07 20:36:33 +0500

    Load first chapter when click the course completed

--- app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
@@ -36,7 +36,7 @@ const CourseSidebarItem = ({
       className={cn(
         "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition hover:text-slate-600 hover:bg-slate-300/20",
         isActive &&
-          "text-slate-700 bg-spate-200/20 hover:bg-slate-200/20 hover:text-slate-700",
+          "text-slate-700 bg-slate-200/20 hover:bg-slate-200/20 hover:text-slate-700",
         isCompleted && "text-emerald-700 hover:text-emerald-700",
         isCompleted && isActive && "bg-emerald-200/20"
       )}
--- app/(course)/courses/[courseId]/page.tsx
+++ app/(course)/courses/[courseId]/page.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-const CourseIdPage = () => {
-  return <div>CoursePage</div>;
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const { courseId } = params;
+  const course = await db.course.findUnique({
+    where: {
+      id: courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+  if (!course) {
+    return redirect("/");
+  }
+
+  return redirect(`/courses/${courseId}/chapters/${course.chapters[0].id}`);
 };
 
 export default CourseIdPage;
