bf77a18f752145f74562dcd61f3d06e6d2dbbcd1
Tharuka Deshan <tharuka1993deshan@gmail.com>
2023-10-08 18:59:33 +0500

    Enabled video player in chapter

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import axios from "axios";
+import MuxPlayer from "@mux/mux-player-react";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { useConfettiStore } from "@/hooks/use-confetti-store";
+import { Loader2, Lock } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface VideoPlayerProps {
+  chapterId: string;
+  courseId: string;
+  title: string;
+  nextChapterId: string | undefined;
+  playbackId: string;
+  isLocked: boolean;
+  completeOnEnd: boolean;
+}
+
+const VideoPlayer = ({
+  chapterId,
+  courseId,
+  title,
+  nextChapterId,
+  playbackId,
+  isLocked,
+  completeOnEnd,
+}: VideoPlayerProps) => {
+  const [isReady, setIsReady] = useState<boolean>(false);
+
+  return (
+    <div className="relative aspect-video">
+      {!isReady && !isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
+          <Loader2 className="h-8 2-8 animate-spin text-secondary" />
+        </div>
+      )}
+
+      {isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+          <Lock className="h-8 w-8" />
+          <p className="text-sm">This chapter is locked</p>
+        </div>
+      )}
+
+      {!isLocked && (
+        <MuxPlayer
+          title={title}
+          className={cn(!isReady && "hidden")}
+          onCanPlay={() => setIsReady(true)}
+          onEnded={() => {}}
+          autoPlay
+          playbackId={playbackId}
+        />
+      )}
+    </div>
+  );
+};
+
+export default VideoPlayer;
--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,14 +1,106 @@
+import { getChapter } from "@/actions/get-chapter";
+import Banner from "@/components/banner";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
+import VideoPlayer from "./_components/video-player";
+import CourseEnrollButton from "./_components/course-enroll-button";
+import { Separator } from "@/components/ui/separator";
+import Preview from "@/components/preview";
+import { File } from "lucide-react";
 
-const ChapterIdPage = ({
+const ChapterIdPage = async ({
   params,
 }: {
   params: { courseId: string; chapterId: string };
 }) => {
   const { chapterId, courseId } = params;
+
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    chapter,
+    course,
+    muxData,
+    nextChapter,
+    attachments,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    courseId,
+    chapterId,
+  });
+
+  if (!chapter || !course) {
+    return redirect("/");
+  }
+
+  const isLocked = !chapter.isFree && !purchase;
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+
   return (
     <div>
-      {courseId} | {chapterId}
+      {userProgress?.isCompleted && (
+        <Banner variant="success" label="You already completed this chapter." />
+      )}
+
+      {isLocked && (
+        <Banner
+          variant="warning"
+          label="You need to purchase this course to watch this chapter."
+        />
+      )}
+
+      <div className="flex flex-col max-w-4xl mx-auto pb-20">
+        <div className="p-4">
+          <VideoPlayer
+            chapterId={chapterId}
+            courseId={courseId}
+            title={chapter.title!}
+            nextChapterId={nextChapter?.id}
+            playbackId={muxData?.playbackId!}
+            isLocked={isLocked}
+            completeOnEnd={completeOnEnd}
+          />
+        </div>
+        <div>
+          <div className="p-4 flex flex-col md:flex-row items-center justify-between">
+            <h2 className="text-2xl font-semibold mb-2">{chapter.title}</h2>
+            {purchase ? (
+              // TODO: Add Course Progress Button
+              ""
+            ) : (
+              <CourseEnrollButton courseId={courseId} price={course.price!} />
+            )}
+          </div>
+          <Separator />
+          <div>
+            <Preview value={chapter.description!} />
+          </div>
+          {!!attachments.length && (
+            <>
+              <Separator />
+              <div className="p-4">
+                {attachments.map((attachment) => (
+                  <a
+                    href={attachment.url}
+                    key={attachment.id}
+                    target="_blank"
+                    className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline"
+                  >
+                    <File />
+                    <p className="line-clamp-1">{attachment.name}</p>
+                  </a>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
